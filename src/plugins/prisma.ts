e05ecd46687e0af0f621bc596e94ce5de476f691
Maharshi Pandya <maharshipandya1999@gmail.com>
2026-01-02 06:01:55 -0500

    Fixed prisma plugin

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,17 +1,21 @@
+import fp from "fastify-plugin";
 import { env } from "../config/env.js";
 import { PrismaClient } from "../../generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 import type { FastifyInstance } from "fastify";
 
-export async function prismaPlugin(app: FastifyInstance) {
-  const connectionString = env.DATABASE_URL;
+export const prismaPlugin = fp(async (app: FastifyInstance) => {
+  const adapter = new PrismaPg({
+    connectionString: env.DATABASE_URL,
+  });
 
-  const adapter = new PrismaPg({ connectionString });
   const prisma = new PrismaClient({ adapter });
+
+  await prisma.$connect();
 
   app.decorate("prisma", prisma);
 
   app.addHook("onClose", async () => {
     await prisma.$disconnect();
   });
-}
+});
